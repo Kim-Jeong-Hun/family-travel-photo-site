5a982d10e564e36c7367a05e98dcbefe9332e626
BLEAX <rlawjdgns135@naver.com>
2025-05-01 18:16:13 +0900

    인트로 애니메이션 구현
    웹사이트에 사용할 아이콘 다운로드(front/public/Transportation Glyph Icons Collection) + 홈페이지 인트로 애니메이션 구현

---
+++ front/src/app/components/IntroAnimation.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import "@/app/styles/IntroAnimation.css"; // CSS 파일 추가
+
+function IntroAnimation() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % 3); // 0, 1, 2 반복
+    }, 2000); // 1초 Fade-in + 1초 Fade-out
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const images = [
+    "/Transportation Glyph Icons Collection/air-airplane-dotted-svgrepo-com.svg",
+    "/Transportation Glyph Icons Collection/big-boat-boats-svgrepo-com.svg",
+    "/Transportation Glyph Icons Collection/light-lights-rail-svgrepo-com.svg",
+  ];
+
+  return (
+    <div className="intro-background">
+      <div className="animation-container">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`icon-${index}`}
+            className={`svg-image ${currentIndex === index ? "visible" : ""}`}
+          />
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default IntroAnimation;
