5fd3e5b1242421cfb2676fbb6029d239b58e8358
Kim Jeong Hun <rlawjdgns135@naver.com>
2025-12-11 18:06:14 +0900

    서버 부분 초기화
    서버 부분 초기화

---
+++ practice/@post/default.js
@@ -0,0 +1,5 @@
+function PostDefault() {
+    return null;
+}
+
+PostDefault();
---
+++ practice/@post/post/page.js
@@ -0,0 +1,13 @@
+"use client";
+import { React } from 'react';
+import PostModal from '../../../components/PostModal';
+
+function Write_page() {
+    return (
+        <div>
+            <PostModal />
+        </div>
+    );
+}
+
+export default Write_page;
