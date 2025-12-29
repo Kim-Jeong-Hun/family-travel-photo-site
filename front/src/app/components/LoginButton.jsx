a4d0d5a74115b0c88cdf10427d64a89144cc219a
KimJeonghun <rlawjdgns135@naver.com>
2025-12-29 17:44:45 +0900

    메인 페이지에 로그인/로그아웃 버튼 구현

---
+++ front/src/app/components/LoginButton.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+function LoginButton() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const router = useRouter();
+    
+    
+    useEffect(() => {
+        const token = localStorage.getItem('accessToken');
+        if(token) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const handleClick = () => {
+        if(isLoggedIn) { // 로그아웃 버튼 클릭 시
+            localStorage.removeItem('accessToken');
+            setIsLoggedIn(false);
+            alert('성공적으로 로그아웃되었습니다.');
+            router.refresh(); // 페이지 상태 새로고침
+        } else { // 로그인 버튼 클릭 시
+            router.push('/login');
+        }
+    }
+
+    return (
+        <>
+            <input
+                type="button"
+                onClick={handleClick}
+                value={isLoggedIn ? '로그아웃' : '로그인'}
+                className="absolute right-[25px] text-center w-[100px] h-[40px] bg-[#ffe500] border-solid rounded-[30px] text-[16px] font-bold text-black cursor-pointer transition-colors duration-300 ease-in-out hover:bg-[#ffdd00]"
+              />
+        </>
+    );
+}
+
+export default LoginButton;
--- front/src/app/components/MainUpperPart.jsx
+++ front/src/app/components/MainUpperPart.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 import Title from "./Title";
+import LoginButton from "./LoginButton";
 
 function MainUpperPart() {
   return (
     <div className="flex flex-row w-full h-full bg-[#FFFBEF] justify-center items-center">
       <Sidebar />
       <Title />
+      <LoginButton />
     </div>
   );
 }
