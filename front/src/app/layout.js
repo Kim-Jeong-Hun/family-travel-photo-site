8feacb3a11ae6cd1132ec27b957f81cb43d8290a
KimJeonghun <rlawjdgns135@naver.com>
2025-12-29 14:18:44 +0900

    Vercel Web Analytics 추가
    - 실제 페이지 방문 수 추적을 위해 Vercel Web Analytics 추가

--- front/src/app/layout.js
+++ front/src/app/layout.js
@@ -1,8 +1,10 @@
+import { Analytics } from '@vercel/analytics/next';
 import './styles/globals.css';
+
 /*
 Tailwind CSS에서는 body와 html의 기본 마진을 제거하기 위해 일반적으로 글로벌 스타일을 작성합니다.
 Tailwind 자체 클래스는 html, body 같은 태그에 직접 적용할 수 없기 때문에, 
-아래처럼 globals.css 파일에 작성하는 방식이 일반적입니다.
+globals.css 파일에 작성하는 방식이 일반적입니다.
 */
 
 
@@ -13,9 +15,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang='ko'>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
