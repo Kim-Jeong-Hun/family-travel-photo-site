e0658723f8af17b4a1e7e43c609d89daefccbb44
KimJeonghun <rlawjdgns135@naver.com>
2026-01-02 17:11:33 +0900

    myPage, settings 페이지 제작 시작

--- front/src/app/myPage/page.js
+++ front/src/app/myPage/page.js
@@ -1,9 +1,12 @@
-import React from 'react';
+"use client";
+import {useState, React} from 'react';
 
 function My_page() {
+    const [isUser, setIsUser] = useState("사용자");
+
     return (
         <div>
-            마이 페이지
+            <p><h4>{isUser}님, 환영합니다!!</h4></p>
         </div>
     );
 }
--- front/src/app/settings/page.js
+++ front/src/app/settings/page.js
@@ -1,12 +1,43 @@
-import { useState } from 'react';
+"use client";
+import { useState, React } from 'react';
 
 function Settings_page() {
     
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+
+
+    /* html 태그 만든 후 -> react로 변환 -> css 적용 -> javascript 및 이벤트 핸들러, 라이브러리 적용 */
     return (
-        <>
-            <input type="radio"></input>
-            <input type="submit">저장하기</input>
-        </>
+        <div>
+            <div>
+                <form action="post">
+                    <div>
+                        <p><h3>테마</h3></p>
+                        <input type="radio" name="theme"/>
+                        <input type="radio" name="theme"/>
+                        <input type="radio" name="theme"/>
+                    </div>
+                    <div>
+                        <p><h3>기본 언어 설정</h3></p>
+                        <select name="languages" id="lang">
+                            <option value="korean">한국어</option>
+                            <option value="english">English</option>
+                            <option value="japanese">日本語</option>
+                            <option value="spanish">español</option>
+                        </select>
+                        <input type="submit" value="저장하기"/>
+                    </div>
+                </form>
+                    <div class="">
+                        <p><h4>About</h4></p>
+                        <p><h4>App Version</h4></p>
+                        <p><h4>1.0.0</h4></p>
+                    </div>
+            </div>
+        </div>
     );
 }
 
